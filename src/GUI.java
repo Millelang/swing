7e6ce07211bfe6ae21d7a4f8dcf3aa5365244d1d
Millelang <Milton.langstrom@elev.ga.ntig.se>
2023-03-31 11:05:26 +0200

    inget funkar..

--- src/GUI.java
+++ src/GUI.java
@@ -1,42 +1,120 @@
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.ButtonGroup;
+import javax.swing.JMenuBar;
+import javax.swing.KeyStroke;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.*;
 
 public class GUI {
     private JPanel panel1;
     private JButton button2;
     private JButton button3;
-    private JTextArea textArea1;
+    private JTextArea textArea2;
     private JButton button4;
     private JButton button5;
     private JButton button6;
+    private JTextArea textArea1;
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("GUI");
-        GUI gui = new GUI();
-        frame.setContentPane(new GUI().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        gui.save();
+    public JMenuBar createMenuBar() {
+        JMenuBar menuBar;
+        JMenu menu, submenu;
+        JMenuItem menuItem;
+        JRadioButtonMenuItem rbMenuItem;
+        JCheckBoxMenuItem cbMenuItem;
 
-    }
-public GUI(){
-        button2.addActionListener(new ActionListener() {
+        //Create the menu bar.
+        menuBar = new JMenuBar();
+
+        //Build the first menu.
+        menu = new JMenu("Arkiv");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "The only menu in this program that has menu items");
+        menuBar.add(menu);
+
+        //a group of JMenuItems
+        menuItem = new JMenuItem("Spara",
+                KeyEvent.VK_T);
+        //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "Sparar");
+        menuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //System.out.println("test save");
+                save();
+            }
+        });
+        menu.add(menuItem);
+        menuItem = new JMenuItem("Öppna",
+                KeyEvent.VK_T);
+        //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "Öppnar");
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //System.out.println("test open");
 
-        String filename = "info.txt";
+                open();
+            }
+        });
+        menu.add(menuItem);
+
+
+
+        //a group of radio button menu items
+
+
+
+
+        //a group of check box menu items
+
+
+
+        //a submenu
+
+
+        //Build second menu in the menu bar.
+        menu = new JMenu("Another Menu");
+        menu.setMnemonic(KeyEvent.VK_N);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "This menu does nothing");
+        menuBar.add(menu);
+
+        return menuBar;
+    }
+
+    public void open(){
+        String filename = "text.txt";
         BufferedReader in = null;
-try {
+        try {
             in = new BufferedReader(new FileReader(filename));
         } catch (FileNotFoundException ex) {
             textArea1.setText("");
             return;
         }
         String nextLine = null;
-try {
+        try {
             nextLine = in.readLine();
             while (nextLine != null) {
                 textArea1.append(nextLine +"\n");
@@ -46,9 +124,9 @@ try {
             textArea1.setText("");
         }
     }
-}
-    );
-}
+
+
+
 
     public void save() {
         String filename = "text.txt";
@@ -58,9 +136,22 @@ try {
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null,"Failed to Save!");
         }
-        out.println("Hello World");
+        System.out.println(textArea1.getText());
+        out.println(textArea1.getText());
         out.flush();
         out.close();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GUI");
+        GUI gui = new GUI();
+        frame.setContentPane(new GUI().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setJMenuBar(gui.createMenuBar());
+        frame.pack();
+        frame.setVisible(true);
+
+
     }
 }
 
